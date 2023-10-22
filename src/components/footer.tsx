cf598e086feed97becee79945cddbab4a6f33475
lilyanb <lilyan.bastien@gmail.com>
2023-10-22 16:59:27 +0200

    add structure

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <div className="w-full px-6 border-t border-t-gray-700 py-2 text-center">
+            Explore Optimisic blockchain with{" "}
+            <Link
+                href="https://github.com/lilyanB/DataCommas"
+                target="_blank"
+                className="text-blue-500 hover:text-blue-600"
+            >
+                us
+            </Link>
+            !
+        </div>
+    );
+}
